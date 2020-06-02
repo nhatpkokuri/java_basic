5e39c05091886cd38e6299c40106c63bfda79251
nhatpham <nhatpham.kokuri@gmail.com>
2020-06-02 12:22:51 +0900

    java variable

---
+++ hello.java
@@ -0,0 +1,14 @@
+/**
+ * hello
+ */
+public class hello {
+
+    public static void main(String[] args) {
+        String hensu = "java";
+        int hensu1 = 100+100;
+        String hensu2 = "100+100";
+        System.out.println(hensu);
+        System.out.println(hensu1);
+        System.out.println(hensu2);
+    }
+}
