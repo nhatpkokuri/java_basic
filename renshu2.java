35d2953b5df57e30d8739f9dfca27dc8b2df7b60
nhatpham <nhatpham.kokuri@gmail.com>
2020-06-03 15:20:39 +0900

    Java Scanner Renshu 2

---
+++ renshu2.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class renshu2 {
+    public static void main(String[] args) {
+        System.out.println("あなた何歳ですか　？");
+        Scanner scan1 = new Scanner(System.in);
+        System.out.println("あなたのなは　：");
+        Scanner scan2 = new Scanner(System.in);
+        int num = scan1.nextInt();
+        String txt = scan2.next();
+        System.out.println(txt + "さん"+num + "歳") ;
+        scan2.close();
+
+
+
+    }
+}
