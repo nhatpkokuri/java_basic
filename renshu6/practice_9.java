f1a39bda4043609b678414dd155f439db8618d37
nhatpham <nhatpham.kokuri@gmail.com>
2020-06-15 15:37:58 +0900

    config practice_9

--- renshu6/practice_9.java
+++ renshu6/practice_9.java
@@ -7,25 +7,15 @@ public class practice_9 {
             System.out.println("月、火、水、木、金、土、日一つ入力ください："); 
             String txt = nc.next();
             switch (txt) {
-            case "月":
+            case "月曜日":
+            case "水曜日":
+            case "木曜日":
+            case "土曜日":
                 System.out.println("ようやくが可能");    
                 break;
-            case "火":
-                System.out.println("休診");
-                break;
-            case "水":
-                System.out.println("ようやくが可能");
-                break;
-            case "木":
-                System.out.println("ようやくが可能");
-                break;
-            case "金":
-                System.out.println("休診");
-                break;
-            case "土":
-                System.out.println("ようやくが可能");
-                break;
-            case "日":
+            case "日曜日":
+            case "金曜日":
+            case "火曜日":
                 System.out.println("休診");
                 break;
             default:
